5f73c7caf2916149ed456d324fad3cd26c07fca5
kevin@kevin <kevin1kipkurui@gmail.com>
2022-06-14 12:39:09 +0300

    commit tree 8c005922ddbbdfeb471f3284482d259982fb9456

---
+++ index.js
@@ -0,0 +1,20 @@
+let alert1=document.getElementById("btn1")
+alert1.addEventListener("click",alertFunction)
+
+
+
+function alertFunction(){
+    alert("I have been clicked")
+}
+let alert2=document.getElementById("btn2")
+alert2.addEventListener("click",alertFunction)
+
+function alertFunction(){
+    alert("i have ben clicked again")
+}
+let alert3=document.getElementById("btn3")
+alert3.addEventListener("click",alertFunction)
+
+function alertFunction(){
+    alert ("i have been clicked now again")
+}
